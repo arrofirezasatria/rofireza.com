eb56996ed146ed70178977d02568bf9f6a9b86aa
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-03-15 19:07:13 +0700

    Edit Footer

--- components/ContainerHero.js
+++ components/ContainerHero.js
@@ -1,11 +1,35 @@
 import React from "react";
+import Head from "next/head";
 import AppBarHero from "../components/AppBar";
+import Footer from "../components/Footer";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 export default function ContainerHero({ children }) {
   return (
     <div>
+      <Head>
+        {/* <title>{meta.title}</title> */}
+        {/* <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
+        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} /> */}
+      </Head>
       <AppBarHero />
-      <main>{children}</main>
+      <Container component="main" maxWidth="md">
+        {children}
+      </Container>
+      <Footer />
     </div>
   );
 }
--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import Box from '@mui/material/Box'
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
 
 export default function Footer() {
   return (
-    <Box>
-        This is Footer
-    </Box>
-  )
+    <Container component="footer" maxWidth="md">
+      <Divider variant="middle" />
+      <Grid container>
+        <Grid item>a</Grid>
+        <Grid item>b</Grid>
+        <Grid item>c</Grid>
+      </Grid>
+      This is Footer
+    </Container>
+  );
 }
