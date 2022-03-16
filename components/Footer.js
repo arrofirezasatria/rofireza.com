8249706497a872dc80ae261e1e0972489be25a09
Arrofi Reza <arrofirezasatria@gmail.com>
2022-03-16 07:35:27 +0700

    footer and index

--- components/Footer.js
+++ components/Footer.js
@@ -3,17 +3,41 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Link from "@mui/material/Link";
+import { alpha, styled } from "@mui/material/styles";
+
+const StyledLink = styled((props) => <Link underline="none" {...props} />)(
+  ({ theme }) => ({})
+);
 
 export default function Footer() {
   return (
-    <Container component="footer" maxWidth="md">
-      <Divider variant="middle" />
-      <Grid container>
-        <Grid item>a</Grid>
-        <Grid item>b</Grid>
-        <Grid item>c</Grid>
+    <Container component="footer" maxWidth="md" sx={{ mb: 2 }}>
+      <Divider variant="middle" sx={{ mb: 4 }} />
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Stack>
+            <StyledLink>Home</StyledLink>
+            <StyledLink>About</StyledLink>
+            <StyledLink>Newsletter</StyledLink>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <Stack>
+            <StyledLink>Twitter</StyledLink>
+            <StyledLink>Youtube</StyledLink>
+            <StyledLink>Github</StyledLink>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <Stack>
+            <StyledLink underline="none">Uses</StyledLink>
+            <StyledLink href="#">Guestbook</StyledLink>
+            <StyledLink>Tweets</StyledLink>
+          </Stack>
+        </Grid>
       </Grid>
-      This is Footer
     </Container>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,25 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ContainerHero from "../components/ContainerHero";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
     <ContainerHero>
-      <div>adadas</div>
+      <Box
+        sx={{
+          height: "400px",
+          display: "flex",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h6" variant="h6">
+          rofireza.io
+        </Typography>
+      </Box>
     </ContainerHero>
   );
 }
