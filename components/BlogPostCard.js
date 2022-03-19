7c18a0286c25c18d9bd174062ef1c1b3e70ec1ef
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-03-19 13:04:06 +0700

    edit blogpost card

--- components/BlogPostCard.js
+++ components/BlogPostCard.js
@@ -2,28 +2,30 @@ import React from "react";
 import Link from "next/link";
 import MuiLink from "@mui/material/Link";
 import Box from "@mui/material/Box";
+import { Stack, Typography } from "@mui/material";
 
 export default function BlogPostCard({ title, slug, gradient }) {
   return (
     <Link href={"#"}>
-      <MuiLink>
+      <a>
         <Box
           sx={{
             display: "flex",
             width: "100%",
             height: "200px",
-            border: "1px solid lightgray",
+            border: "4px solid lightgray",
             borderRadius: "6px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignContent: "center",
-            alignItems: "center",
-            verticalAlign: "center",
+            padding: 2,
           }}
         >
-          <Box>abs</Box>
+          <Stack
+            sx={{ justifyContent: "space-between", alignItems: "flex-start" }}
+          >
+            <Typography>Title</Typography>
+            <Box sx={{}}>viewcount</Box>
+          </Stack>
         </Box>
-      </MuiLink>
+      </a>
     </Link>
   );
 }
