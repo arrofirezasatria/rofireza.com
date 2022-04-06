7e9a76de55a3b1f0ab4142a64626389b67b7280f
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-04-06 17:05:58 +0700

    just update

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,11 +2,45 @@ import { Container } from '@mui/material';
 import React from 'react'
 import ContainerHero from '../components/ContainerHero';
 import Box from '@mui/material/Box'
+import { fsync } from 'fs';
 
-export default function Blog() {
+export default function Blog({posts}) {
   return (
     <ContainerHero>
       <Box>blog</Box>      
+  {posts.map((data)=>{
+    return(
+      <div>
+        {data.slug}
+        {data.summary}
+        {data.publishedAt}
+        </div>
+      
+      )
+  })}
     </ContainerHero>
+    
   )
+}
+
+export function getStaticProps(){
+  const posts = [
+    {
+      'slug':'blog 1',
+      'title':'judul 1',
+      'summary': 'Velit aliquip Lorem ut ut Lorem nisi sunt fugiat.',
+      'publishedAt': 'yesterday'
+    },
+    {
+      'slug':'blog 2',
+      'title':'judul 2',
+      'summary': 'Ipsum voluptate reprehenderit adipisicing nisi.',
+      'publishedAt': 'Just Now'
+    }
+  ]
+
+
+  return {
+    props: {posts}
+  }
 }
