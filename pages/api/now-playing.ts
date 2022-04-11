416324e0310443b0f27621cebb74155e68245071
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-04-11 11:10:08 +0700

    fix bug guestbook

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -2,9 +2,18 @@ import { fsync } from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    const response = await getNowPlaying();
-        
+  const response = await getNowPlaying(req, res);
+}
+
+async function getNowPlaying(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+
+  try {
+    return res.status(200).json({ message: "its work", success: true });
+  } catch (error) {
+    return res.status(500).json({ message: "error", success: false });
+  }
 }
