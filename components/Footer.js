44d2248cb9b0cbae35769767c846bcded0eadd62
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-05-11 16:31:45 +0700

    update

--- components/Footer.js
+++ components/Footer.js
@@ -15,86 +15,84 @@ import EmailIcon from "@mui/icons-material/Email";
 import Links from "next/link";
 
 const StyledLink = styled((props) => <Link underline="none" {...props} />)(
-    ({ theme }) => ({})
+  ({ theme }) => ({})
 );
 
 export default function Footer() {
-    return (
-        <Container component="footer" maxWidth="md" sx={{ pb: 8, pt: 2 }}>
-            <Divider sx={{ mb: "40px" }} />
-            <Grid container spacing={2}>
-                <Grid item xs={7}>
-                    <Typography
-                        sx={{
-                            color: blueDark[100],
-                            fontSize: "14px",
-                            fontWeight: 600,
-                        }}
-                    >
-                        Reach me on ,
-                    </Typography>
+  return (
+    <Container component="footer" maxWidth="md" sx={{ pb: 8, pt: 2 }}>
+      <Divider sx={{ mb: "40px" }} />
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <Typography
+            sx={{
+              color: blueDark[100],
+              fontSize: "14px",
+              fontWeight: 600,
+            }}
+          >
+            Reach me on ,
+          </Typography>
 
-                    <Stack direction="row" spacing={1}>
-                        <IconButton
-                            sx={{
-                                height: "14px",
-                                width: "14px",
-                            }}
-                        >
-                            <EmailIcon />
-                        </IconButton>
-                        <IconButton
-                            sx={{
-                                height: "14px",
-                                width: "14px",
-                            }}
-                        >
-                            <EmailIcon />
-                        </IconButton>
-                        <IconButton
-                            sx={{
-                                height: "14px",
-                                width: "14px",
-                            }}
-                        >
-                            <EmailIcon />
-                        </IconButton>
-                    </Stack>
-                    <Typography
-                        sx={{
-                            color: blueDark[300],
-                            fontSize: "14px",
-                            paddingTop: "36px",
-                        }}
-                    >
-                        Join our newsletter for regular updates. No spam ever.
-                    </Typography>
-                    <Box sx={{}}>
-                        <EmailSubscriber />
-                    </Box>
-                </Grid>
-                <Grid item xs={2}>
-                    <Stack spacing={2}>
-                        <StyledLink href="https://www.twitter.com">
-                            Twitter
-                        </StyledLink>
-                        <StyledLink href="https://www.youtube.com">
-                            Youtube
-                        </StyledLink>
-                        <StyledLink href="https://github.com">
-                            Github
-                        </StyledLink>
-                    </Stack>
-                </Grid>
-                <Grid item xs={3}>
-                    <Stack spacing={2}>
-                        <StyledLink href="/">Home</StyledLink>
-                        <StyledLink href="/guestbook">Guestbook</StyledLink>
-                        <StyledLink href="#">Tweets</StyledLink>
-                    </Stack>
-                </Grid>
-            </Grid>
-            <Divider sx={{ pt: "40px" }} />
-        </Container>
-    );
+          <Stack direction="row" spacing={1}>
+            <IconButton
+              sx={{
+                height: "14px",
+                width: "14px",
+              }}
+            >
+              <EmailIcon />
+            </IconButton>
+            <IconButton
+              sx={{
+                height: "14px",
+                width: "14px",
+              }}
+            >
+              <EmailIcon />
+            </IconButton>
+            <IconButton
+              sx={{
+                height: "14px",
+                width: "14px",
+              }}
+            >
+              <EmailIcon />
+            </IconButton>
+          </Stack>
+          <Typography
+            sx={{
+              color: blueDark[300],
+              fontSize: "14px",
+              paddingTop: "36px",
+            }}
+          >
+            Join our newsletter for regular updates. No spam ever.
+          </Typography>
+          <Box sx={{}}>
+            <EmailSubscriber />
+          </Box>
+        </Grid>
+        <Grid item xs={2}>
+          <Stack spacing={2} sx={{ fontSize: "14px", textDecoration: "none" }}>
+            <Typography fontWeight="bold" variant="body2">
+              Products
+            </Typography>
+            <Link underline="none">MUI Core</Link>
+            <Link underline="none">MUI X</Link>
+            <Link underline="none">Templates</Link>
+            <Link underline="none">Design kits</Link>
+          </Stack>
+        </Grid>
+        <Grid item xs={3}>
+          <Stack spacing={2}>
+            <StyledLink href="/">Home</StyledLink>
+            <StyledLink href="/guestbook">Guestbook</StyledLink>
+            <StyledLink href="#">Tweets</StyledLink>
+          </Stack>
+        </Grid>
+      </Grid>
+      <Divider sx={{ pt: "40px" }} />
+    </Container>
+  );
 }
