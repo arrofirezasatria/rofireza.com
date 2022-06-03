551295e05a0ab760ca55b9137f511df827efc633
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-06-03 15:55:18 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,19 @@ import React from "react";
 import { CodeCopyProvider } from "../modules/CodeCopy";
 import { SessionProvider } from "next-auth/react";
 import ThemeContext from "../modules/ThemeContext";
+import { Provider } from "react-redux";
 
 import "../styles/prismokaida.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-    return (
-        <SessionProvider session={session}>
-            <ThemeContext>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeContext>
-        </SessionProvider>
-    );
+  return (
+    <SessionProvider session={session}>
+      <ThemeContext>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeContext>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
