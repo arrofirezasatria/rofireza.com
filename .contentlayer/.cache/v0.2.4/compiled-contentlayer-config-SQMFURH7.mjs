2fbedde026310ba83147dd6f166d7bd123b28d0e
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-06-21 12:45:36 +0700

    not ignore contentlayer generated

---
+++ .contentlayer/.cache/v0.2.4/compiled-contentlayer-config-SQMFURH7.mjs
@@ -0,0 +1,54 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrism from "rehype-prism-plus";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true
+    },
+    date: {
+      type: "date",
+      description: "The date of the post",
+      required: true
+    }
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      rehypePrism,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"]
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-SQMFURH7.mjs.map
