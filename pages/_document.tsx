195eab6990218e5cc2eb7128aa993db0c2fca84b
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-08-13 17:10:06 +0700

    commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 import Box from "@mui/material/Box";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <Box component="body">
-                <Main />
-                <NextScript />
-                <script defer src="/prism.js"></script>
-            </Box>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Box component="body">
+        <Main />
+        <Script strategy="beforeInteractive" src="/prism.js" />
+      </Box>
+    </Html>
+  );
 }
