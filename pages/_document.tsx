a554d28a51595fbfd5b316685b9878976b8788f4
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-08-30 14:58:21 +0700

    add google verivication meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,25 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-import Box from "@mui/material/Box";
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+import Box from '@mui/material/Box'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Box component="body">
-        <Main />
-        <NextScript />
-        <Script strategy="beforeInteractive" src="/prism.js" />
-      </Box>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+                    rel="stylesheet"
+                />
+                <meta
+                    name="google-site-verification"
+                    content="fv1AWO_U0wxEVTabsVDk5RKpITRx5L9TY879fIFJrDg"
+                />
+            </Head>
+            <Box component="body">
+                <Main />
+                <NextScript />
+                <Script strategy="beforeInteractive" src="/prism.js" />
+            </Box>
+        </Html>
+    )
 }
