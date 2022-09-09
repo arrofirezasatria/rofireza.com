029672353c7fa0a59c39cd9b740c2094fa82250d
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-09-09 17:24:11 +0700

    update

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,13 +6,5 @@ import Image from 'next/image'
 import { Box } from '@mui/material'
 
 export default function test() {
-    return (
-        <Box sx={{ position: 'relative' }}>
-            <Image
-                src={'/showcase/original/default-image.jpg'}
-                layout={'fill'}
-                srcSet={''}
-            />
-        </Box>
-    )
+    return <Box sx={{ position: 'relative' }}>ad</Box>
 }
