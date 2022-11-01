f64db828925af618c83c73e69c49080f8dc97fe4
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-11-01 16:29:56 +0700

    feat: add placeholder playlist page

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { DiscordStatus } from '@lib/types/index'
 
 const discord_id = process.env.DISCORD_ID
 const youtube_api = process.env.YOUTUBE_API
@@ -13,16 +12,11 @@ export default async function handler(
 
     const response = await axios
         .get(
-            `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2C%20contentDetails%2C%20status%2C%20id&maxResults=10&playlistId=PLN8AHML34obuWGhlzCszGKG2_9gScbn0Z&key=AIzaSyCE2SJHUPG0kh5Z5ZjavK8KiKpuyzvLHJQ`
+            `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2C%20contentDetails%2C%20status%2C%20id&maxResults=10&playlistId=PLN8AHML34obuWGhlzCszGKG2_9gScbn0Z&key=${youtube_api}`
         )
         .then((res) => res)
 
     return res.status(200).json(response.data)
-    // console.log(response)
-
-    // if (response.status === 204 || response.status > 400) {
-    //     return res.status(200).json({ status: 'offline' })
-    // }
 
     return res.status(200).json({ messsage: 'success' })
 }
--- pages/playlist.tsx
+++ pages/playlist.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import ContainerHero from '@layouts/ContainerHero'
+import axios from 'axios'
 
 export default function playlist() {
-    return <div>playlist</div>
+    return <ContainerHero>asd</ContainerHero>
+}
+
+export async function getStaticProps() {
+    const youtube_api = process.env.YOUTUBE_API
+
+    const response = await axios
+        .get(
+            `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2C%20contentDetails%2C%20status%2C%20id&maxResults=10&playlistId=PLN8AHML34obuWGhlzCszGKG2_9gScbn0Z&key=${youtube_api}`
+        )
+        .then((res) => res)
+
+    console.log(response.data)
+    return {
+        props: {},
+        revalidate: 360,
+    }
 }
